baa762e694d7ab866c634ab10a203d55febe524c
Muhammad Ahmad <mas7897860@gmail.com>
2026-01-01 03:59:28 +0500

    first commit

---
+++ nuvra-wp-core-tools.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:     Nuvra Wp Core Tools
+ * Plugin URI:      https://github.com/ahmad4372/nuvra-task-plugin
+ * Description:     Nuvra skill accessment task
+ * Author:          Muhammad Ahmad
+ * Author URI:      https://github.com/ahmad4372/
+ * Text Domain:     nuvra-wp-core-tools
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Nuvra_Wp_Core_Tools
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
